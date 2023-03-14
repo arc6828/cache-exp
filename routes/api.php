b5834e69fca23d53ddefe886299f24cd28560c39
arc6828 <chavalit.kow@gmail.com>
2023-03-14 18:17:47 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Medium;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::prefix('cache')->group(function () {
+    Route::get('file',function(){
+        $data = Medium::fetch("samkhok","","file");
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    });
+
+    Route::get('memcached',function(){
+        $data = Medium::fetch("samkhok","","memcached");
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    });
+
+    Route::get('database',function(){
+        $data = Medium::fetch("samkhok","","database");
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    });
+
+    Route::get('redis',function(){
+        $data = Medium::fetch("samkhok","","redis");
+        return json_encode($data, JSON_UNESCAPED_UNICODE);
+    });
+});
