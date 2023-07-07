6dd43050c50b263e0f2a4c05b9ac02e764843c25
arc6828 <chavalit.kow@gmail.com>
2023-07-07 13:19:54 +0700

    Test Experiment

--- run.sh
+++ run.sh
@@ -2,13 +2,13 @@
 for i in 10 30 60; do
     for size in 20 200 2000; do
         echo "${i}s / ${size}KB"
-        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/file/${size}KB" >performance/file-${i}s.txt
+        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/file/${size}KB" >performance/file-${i}s-${size}KB.txt
         echo "1. run file successfully"
-        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/mysql/${size}KB" >performance/mysql-${i}s.txt
+        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/mysql/${size}KB" >performance/mysql-${i}s-${size}KB.txt
         echo "2. run mysql successfully"
-        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/redis/${size}KB" >performance/redis-${i}s.txt
+        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/redis/${size}KB" >performance/redis-${i}s-${size}KB.txt
         echo "3. run redis successfully"
-        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/none/${size}KB" >performance/nocache-${i}s.txt
+        wrk -t1 -c1 -d${i}s "http://$(hostname).local/cache-exp/public/api/cache/none/${size}KB" >performance/nocache-${i}s-${size}KB.txt
         echo "4. run nocache successfully"
     done
 done
