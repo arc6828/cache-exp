19a3c7a1982df6abe21e1aa1e8e3595a30a50edb
Chavalit Koweerawong <chavalit.kow@gmail.com>
2023-07-15 10:52:41 +0700

    Update Payload.php

--- app/Models/Payload.php
+++ app/Models/Payload.php
@@ -15,7 +15,7 @@ class Payload extends Model
     {
         // $url = "https://news.google.com/news/rss";
         // $url = url("/payload/dataset-{$size}.json");
-        $url = url("https://raw.githubusercontent.com/arc6828/cache-exp/main/public/payload/dataset-{$size}.json?id={$id}");
+        $url = "https://raw.githubusercontent.com/arc6828/cache-exp/main/public/payload/dataset-{$size}.json?id={$id}";
 
         $data = Cache::store($driver)->remember($url, now()->addDay(), function () use ($url) {
             //FETCH DATA
@@ -32,7 +32,7 @@ class Payload extends Model
     {
         // $url = "https://news.google.com/news/rss";
         // $url = url("/payload/dataset-{$size}.json");        
-        $url = url("https://raw.githubusercontent.com/arc6828/cache-exp/main/public/payload/dataset-{$size}.json?id={$id}");
+        $url = "https://raw.githubusercontent.com/arc6828/cache-exp/main/public/payload/dataset-{$size}.json?id={$id}";
 
         $fileContents = file_get_contents($url);
         return $fileContents;
@@ -42,13 +42,13 @@ class Payload extends Model
     public static function fetch3($size, $id)
     {
         // $url = "https://news.google.com/news/rss";
-        // $url = url("/payload/dataset-{$size}.json");        
+        $url = url("/payload/dataset-{$size}.json");        
         // $url = url("https://raw.githubusercontent.com/arc6828/cache-exp/main/public/payload/dataset-{$size}.json?id={$id}");
-        $filename = public_path()."/payload/dataset-{$size}.json";
-        $myfile = fopen($filename, "r") or die("Unable to open file!");
-        $fileContents = fread($myfile, filesize($filename));
-        fclose($myfile);
-        // $fileContents = file_get_contents($url);
+        // $filename = public_path()."/payload/dataset-{$size}.json";
+        // $myfile = fopen($filename, "r") or die("Unable to open file!");
+        // $fileContents = fread($myfile, filesize($filename));
+        // fclose($myfile);
+        $fileContents = file_get_contents($url);
         return $fileContents;
         // $data = json_decode($fileContents);
         // return $data;
