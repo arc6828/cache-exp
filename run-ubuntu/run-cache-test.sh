fefc0627969bad58148bb229c532aafb6776c47f
arc6828 <chavalit.kow@gmail.com>
2023-07-19 14:42:00 +0700

    latency

--- run-ubuntu/run-cache-test.sh
+++ run-ubuntu/run-cache-test.sh
@@ -14,13 +14,13 @@
 # sleep 10
 # measure state
 for d in 1000; do
-    for size in 200; do
+    for size in 2 20 200 2000; do
         echo "size : ${size} / amount : ${d}"
-        curl "http://localhost:8000/api/cache/test/file/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/file-${size}.txt
+        curl "http://localhost:8000/api/cache/test/file/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/file-${size}KB.json
         echo "1. run file successfully"
-        curl "http://localhost:8000/api/cache/test/database/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/mysql-${size}.txt
+        curl "http://localhost:8000/api/cache/test/database/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/mysql-${size}.json
         echo "2. run mysql successfully"
-        curl "http://localhost:8000/api/cache/test/redis/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/redis-${size}.txt
+        curl "http://localhost:8000/api/cache/test/redis/${size}KB/${d}" >/var/www/cache-exp/performance/cache-latency/redis-${size}.json
         echo "3. run redis successfully"
     done
 done
